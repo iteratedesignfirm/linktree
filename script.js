188027f6489f57d4ee961e4e99b45084781b6e4c
adewale <obadunadewale1234@gmail.com>
2024-08-09 09:06:04 +0100

    i38

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 // @ts-nocheck
-const input = document.getElementById('myInput');
-        
-input.addEventListener('focus', () => {
-    input.placeholder = '';
-});
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('subscriptionForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
 
-input.addEventListener('blur', () => {
-    if (input.value === '') {
-        input.placeholder = 'Enter text here';
+    const email = document.getElementById('email').value;
+
+    try {
+      const response = await fetch('https://resourcekit-server.onrender.com/addEmailToMailingList', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email })
+      });
+
+      if (response.ok) {
+        alert('Subscription successful!');
+      } else {
+        alert('Subscription failed.');
+      }
+    } catch (error) {
+      console.error('Error details:', error);
+      alert('An error occurred.');
     }
+  });
 });
